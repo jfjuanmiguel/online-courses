4c01c7a297722070b10e8232e22cd992c35a06f8
Juan Jimenez <jimenez.f.juan.m@gmail.com>
2018-10-13 15:57:18 -0400

    Add Services for GitHub and Facebook - Login/Register

--- config/services.php
+++ config/services.php
@@ -36,4 +36,15 @@ return [
         'secret' => env('STRIPE_SECRET'),
     ],
 
+    //socialite
+    'github' => [
+        'client_id' => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'redirect' => '/login/github/callback',
+    ],
+    'facebook' => [
+        'client_id' => env('FACEBOOK_CLIENT_ID'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+        'redirect' => '/login/facebook/callback',
+    ],
 ];
