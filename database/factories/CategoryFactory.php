76b1b7c3435351998eef82c3000f1ea1ec91621a
Juan Jimenez <jimenez.f.juan.m@gmail.com>
2018-10-13 16:31:50 -0400

    Create Model Factories

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -3,7 +3,19 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Category::class, function (Faker $faker) {
+    $categories = array(
+        'PHP',
+        'JAVASCRIPT',
+        'JAVA',
+        'DISEÑO WEB',
+        'SERVIDORES',
+        'MYSQL',
+        'NOSQL',
+        'BIGDATA',
+        'AMAZON WEB SERVICES'
+    );
     return [
-        //
+        'name' => $faker->randomElement($categories),
+        'description' => $faker->sentence,
     ];
 });
